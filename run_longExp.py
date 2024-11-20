75c1735b9cf1639c93cf2d0f885c467f2f9e4faf
anshul013 <ultimatenanshul@gmail.com>
2024-11-20 18:45:49 +0100

    Resolved individual multiple instances

--- run_longExp.py
+++ run_longExp.py
@@ -51,7 +51,7 @@ parser.add_argument('--label_len', type=int, default=48, help='start token lengt
 parser.add_argument('--pred_len', type=int, default=96, help='prediction sequence length')
 
 # DLinear
-parser.add_argument('--individual', action='store_true', default=False, help='DLinear: a linear layer for each variate(channel) individually')
+# parser.add_argument('--individual', action='store_true', default=False, help='DLinear: a linear layer for each variate(channel) individually')
 # Formers 
 parser.add_argument('--embed_type', type=int, default=0, help='0: default 1: value embedding + temporal embedding + positional embedding 2: value embedding + temporal embedding 3: value embedding + positional embedding 4: value embedding')
 parser.add_argument('--dec_in', type=int, default=7, help='decoder input size')
