839396075a2161c4aa770ac58cbc3cbbe5e77377
anshul013 <ultimatenanshul@gmail.com>
2025-01-23 18:00:09 +0100

    Updated bash files of rest of ETT datasets

--- ETTm1.sh
+++ ETTm1.sh
@@ -6,28 +6,30 @@ if [ ! -d "./logs/LongForecasting" ]; then
     mkdir ./logs/LongForecasting
 fi
 
-if [ ! -d "./logs/LongForecasting/TSMixerCCM" ]; then
-    mkdir ./logs/LongForecasting/TSMixerCCM
+if [ ! -d "./logs/LongForecasting/TSMixerH" ]; then
+    mkdir ./logs/LongForecasting/TSMixerH
 fi
-seq_len=512
-model_name=TSMixerCCM
+seq_len=336
+model_name=TSMixerH
 dataset=ETTm1
 num_channels=7
 
-#Best configuration for ETTm2 and 96 frames horizon
 pred_len=96
 python3 -u run_longExp.py \
   --activation 'relu' \
-  --dropout 0.9\
-  --hidden_size 16\
-  --num_clusters 10\
-  --num_blocks 6 \
+  --dropout 0.9 \
+  --num_clusters 3 \
+  --clustering_method 'kmeans' \
+  --n_layers 6 \
+  --d_ff 16 \
+  --in_len $seq_len \
+  --out_len $pred_len \
   --is_training 1 \
   --root_path ./dataset/ \
   --data_path $dataset.csv \
   --model_id $dataset'_'$seq_len'_'$pred_len \
   --model $model_name \
-  --data ETTm1 \
+  --data ETTh1 \
   --features M \
   --seq_len $seq_len \
   --pred_len $pred_len \
@@ -35,22 +37,24 @@ python3 -u run_longExp.py \
   --des 'Exp' \
   --patience 5 \
   --train_epochs 100 \
-  --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
+  --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log
 
-#Best configuration for ETTm2 and 192 frames horizon
 pred_len=192
 python3 -u run_longExp.py \
   --activation 'relu' \
-  --dropout 0.9\
-  --hidden_size 32\
-  --num_clusters 10\
-  --num_blocks 4 \
+  --dropout 0.9 \
+  --num_clusters 3 \
+  --clustering_method 'kmeans' \
+  --n_layers 4 \
+  --d_ff 32 \
+  --in_len $seq_len \
+  --out_len $pred_len \
   --is_training 1 \
   --root_path ./dataset/ \
   --data_path $dataset.csv \
   --model_id $dataset'_'$seq_len'_'$pred_len \
   --model $model_name \
-  --data ETTm1 \
+  --data ETTh1 \
   --features M \
   --seq_len $seq_len \
   --pred_len $pred_len \
@@ -60,20 +64,22 @@ python3 -u run_longExp.py \
   --train_epochs 100 \
   --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
 
-#Best configuration for ETTm2 and 336 frames horizon
 pred_len=336
 python3 -u run_longExp.py \
   --activation 'relu' \
-  --dropout 0.9\
-  --hidden_size 64\
-  --num_clusters 10\
-  --num_blocks 4 \
+  --dropout 0.9 \
+  --num_clusters 3 \
+  --clustering_method 'kmeans' \
+  --n_layers 4 \
+  --d_ff 64 \
+  --in_len $seq_len \
+  --out_len $pred_len \
   --is_training 1 \
   --root_path ./dataset/ \
   --data_path $dataset.csv \
   --model_id $dataset'_'$seq_len'_'$pred_len \
   --model $model_name \
-  --data ETTm1 \
+  --data ETTh1 \
   --features M \
   --seq_len $seq_len \
   --pred_len $pred_len \
@@ -83,20 +89,22 @@ python3 -u run_longExp.py \
   --train_epochs 100 \
   --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
 
-#Best configuration for ETTm2 and 720 frames horizon
 pred_len=720
 python3 -u run_longExp.py \
   --activation 'relu' \
-  --dropout 0.1\
-  --hidden_size 16\
-  --num_clusters 10\
-  --num_blocks 4 \
+  --dropout 0.9 \
+  --num_clusters 3 \
+  --clustering_method 'kmeans' \
+  --n_layers 4 \
+  --d_ff 16 \
+  --in_len $seq_len \
+  --out_len $pred_len \
   --is_training 1 \
   --root_path ./dataset/ \
   --data_path $dataset.csv \
   --model_id $dataset'_'$seq_len'_'$pred_len \
   --model $model_name \
-  --data ETTm1 \
+  --data ETTh1 \
   --features M \
   --seq_len $seq_len \
   --pred_len $pred_len \
@@ -105,3 +113,95 @@ python3 -u run_longExp.py \
   --patience 5 \
   --train_epochs 100 \
   --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
+
+# #Best configuration for ETTm2 and 96 frames horizon
+# pred_len=96
+# python3 -u run_longExp.py \
+#   --activation 'relu' \
+#   --dropout 0.9\
+#   --hidden_size 16\
+#   --num_clusters 10\
+#   --num_blocks 6 \
+#   --is_training 1 \
+#   --root_path ./dataset/ \
+#   --data_path $dataset.csv \
+#   --model_id $dataset'_'$seq_len'_'$pred_len \
+#   --model $model_name \
+#   --data ETTm1 \
+#   --features M \
+#   --seq_len $seq_len \
+#   --pred_len $pred_len \
+#   --enc_in $num_channels \
+#   --des 'Exp' \
+#   --patience 5 \
+#   --train_epochs 100 \
+#   --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
+
+# #Best configuration for ETTm2 and 192 frames horizon
+# pred_len=192
+# python3 -u run_longExp.py \
+#   --activation 'relu' \
+#   --dropout 0.9\
+#   --hidden_size 32\
+#   --num_clusters 10\
+#   --num_blocks 4 \
+#   --is_training 1 \
+#   --root_path ./dataset/ \
+#   --data_path $dataset.csv \
+#   --model_id $dataset'_'$seq_len'_'$pred_len \
+#   --model $model_name \
+#   --data ETTm1 \
+#   --features M \
+#   --seq_len $seq_len \
+#   --pred_len $pred_len \
+#   --enc_in $num_channels \
+#   --des 'Exp' \
+#   --patience 5 \
+#   --train_epochs 100 \
+#   --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
+
+# #Best configuration for ETTm2 and 336 frames horizon
+# pred_len=336
+# python3 -u run_longExp.py \
+#   --activation 'relu' \
+#   --dropout 0.9\
+#   --hidden_size 64\
+#   --num_clusters 10\
+#   --num_blocks 4 \
+#   --is_training 1 \
+#   --root_path ./dataset/ \
+#   --data_path $dataset.csv \
+#   --model_id $dataset'_'$seq_len'_'$pred_len \
+#   --model $model_name \
+#   --data ETTm1 \
+#   --features M \
+#   --seq_len $seq_len \
+#   --pred_len $pred_len \
+#   --enc_in $num_channels \
+#   --des 'Exp' \
+#   --patience 5 \
+#   --train_epochs 100 \
+#   --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
+
+# #Best configuration for ETTm2 and 720 frames horizon
+# pred_len=720
+# python3 -u run_longExp.py \
+#   --activation 'relu' \
+#   --dropout 0.1\
+#   --hidden_size 16\
+#   --num_clusters 10\
+#   --num_blocks 4 \
+#   --is_training 1 \
+#   --root_path ./dataset/ \
+#   --data_path $dataset.csv \
+#   --model_id $dataset'_'$seq_len'_'$pred_len \
+#   --model $model_name \
+#   --data ETTm1 \
+#   --features M \
+#   --seq_len $seq_len \
+#   --pred_len $pred_len \
+#   --enc_in $num_channels \
+#   --des 'Exp' \
+#   --patience 5 \
+#   --train_epochs 100 \
+#   --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
--- ETTm2.sh
+++ ETTm2.sh
@@ -6,29 +6,31 @@ if [ ! -d "./logs/LongForecasting" ]; then
     mkdir ./logs/LongForecasting
 fi
 
-if [ ! -d "./logs/LongForecasting/TSMixerCCM" ]; then
-    mkdir ./logs/LongForecasting/TSMixerCCM
+if [ ! -d "./logs/LongForecasting/TSMixerH" ]; then
+    mkdir ./logs/LongForecasting/TSMixerH
 fi
 
-seq_len=512
-model_name=TSMixerCCM
+seq_len=336
+model_name=TSMixerH
 dataset=ETTm2
 num_channels=7
 
-#Best configuration for ETTm2 and 96 frames horizon
 pred_len=96
 python3 -u run_longExp.py \
   --activation 'relu' \
-  --dropout 0.9\
-  --hidden_size 256\
-  --num_clusters 10\
-  --num_blocks 8 \
+  --dropout 0.9 \
+  --num_clusters 3 \
+  --clustering_method 'kmeans' \
+  --n_layers 8 \
+  --d_ff 256 \
+  --in_len $seq_len \
+  --out_len $pred_len \
   --is_training 1 \
   --root_path ./dataset/ \
   --data_path $dataset.csv \
   --model_id $dataset'_'$seq_len'_'$pred_len \
   --model $model_name \
-  --data ETTm2 \
+  --data ETTh1 \
   --features M \
   --seq_len $seq_len \
   --pred_len $pred_len \
@@ -36,22 +38,24 @@ python3 -u run_longExp.py \
   --des 'Exp' \
   --patience 5 \
   --train_epochs 100 \
-  --itr 1 --batch_size 32 --learning_rate 0.001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
+  --itr 1 --batch_size 32 --learning_rate 0.001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log
 
-#Best configuration for ETTm2 and 192 frames horizon
 pred_len=192
 python3 -u run_longExp.py \
   --activation 'relu' \
-  --dropout 0.9\
-  --hidden_size 256\
-  --num_clusters 10\
-  --num_blocks 1 \
+  --dropout 0.9 \
+  --num_clusters 3 \
+  --clustering_method 'kmeans' \
+  --n_layers 1 \
+  --d_ff 256 \
+  --in_len $seq_len \
+  --out_len $pred_len \
   --is_training 1 \
   --root_path ./dataset/ \
   --data_path $dataset.csv \
   --model_id $dataset'_'$seq_len'_'$pred_len \
   --model $model_name \
-  --data ETTm2 \
+  --data ETTh1 \
   --features M \
   --seq_len $seq_len \
   --pred_len $pred_len \
@@ -61,20 +65,22 @@ python3 -u run_longExp.py \
   --train_epochs 100 \
   --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
 
-#Best configuration for ETTm2 and 336 frames horizon
 pred_len=336
 python3 -u run_longExp.py \
   --activation 'relu' \
-  --dropout 0.9\
-  --hidden_size 512\
-  --num_clusters 10\
-  --num_blocks 8 \
+  --dropout 0.9 \
+  --num_clusters 3 \
+  --clustering_method 'kmeans' \
+  --n_layers 8 \
+  --d_ff 512 \
+  --in_len $seq_len \
+  --out_len $pred_len \
   --is_training 1 \
   --root_path ./dataset/ \
   --data_path $dataset.csv \
   --model_id $dataset'_'$seq_len'_'$pred_len \
   --model $model_name \
-  --data ETTm2 \
+  --data ETTh1 \
   --features M \
   --seq_len $seq_len \
   --pred_len $pred_len \
@@ -84,20 +90,22 @@ python3 -u run_longExp.py \
   --train_epochs 100 \
   --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
 
-#Best configuration for ETTm2 and 720 frames horizon
 pred_len=720
 python3 -u run_longExp.py \
   --activation 'relu' \
-  --dropout 0.1\
-  --hidden_size 256\
-  --num_clusters 10\
-  --num_blocks 8 \
+  --dropout 0.1 \
+  --num_clusters 3 \
+  --clustering_method 'kmeans' \
+  --n_layers 8 \
+  --d_ff 256 \
+  --in_len $seq_len \
+  --out_len $pred_len \
   --is_training 1 \
   --root_path ./dataset/ \
   --data_path $dataset.csv \
   --model_id $dataset'_'$seq_len'_'$pred_len \
   --model $model_name \
-  --data ETTm2 \
+  --data ETTh1 \
   --features M \
   --seq_len $seq_len \
   --pred_len $pred_len \
@@ -106,3 +114,95 @@ python3 -u run_longExp.py \
   --patience 5 \
   --train_epochs 100 \
   --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
+
+# #Best configuration for ETTm2 and 96 frames horizon
+# pred_len=96
+# python3 -u run_longExp.py \
+#   --activation 'relu' \
+#   --dropout 0.9\
+#   --hidden_size 256\
+#   --num_clusters 10\
+#   --num_blocks 8 \
+#   --is_training 1 \
+#   --root_path ./dataset/ \
+#   --data_path $dataset.csv \
+#   --model_id $dataset'_'$seq_len'_'$pred_len \
+#   --model $model_name \
+#   --data ETTm2 \
+#   --features M \
+#   --seq_len $seq_len \
+#   --pred_len $pred_len \
+#   --enc_in $num_channels \
+#   --des 'Exp' \
+#   --patience 5 \
+#   --train_epochs 100 \
+#   --itr 1 --batch_size 32 --learning_rate 0.001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
+
+# #Best configuration for ETTm2 and 192 frames horizon
+# pred_len=192
+# python3 -u run_longExp.py \
+#   --activation 'relu' \
+#   --dropout 0.9\
+#   --hidden_size 256\
+#   --num_clusters 10\
+#   --num_blocks 1 \
+#   --is_training 1 \
+#   --root_path ./dataset/ \
+#   --data_path $dataset.csv \
+#   --model_id $dataset'_'$seq_len'_'$pred_len \
+#   --model $model_name \
+#   --data ETTm2 \
+#   --features M \
+#   --seq_len $seq_len \
+#   --pred_len $pred_len \
+#   --enc_in $num_channels \
+#   --des 'Exp' \
+#   --patience 5 \
+#   --train_epochs 100 \
+#   --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
+
+# #Best configuration for ETTm2 and 336 frames horizon
+# pred_len=336
+# python3 -u run_longExp.py \
+#   --activation 'relu' \
+#   --dropout 0.9\
+#   --hidden_size 512\
+#   --num_clusters 10\
+#   --num_blocks 8 \
+#   --is_training 1 \
+#   --root_path ./dataset/ \
+#   --data_path $dataset.csv \
+#   --model_id $dataset'_'$seq_len'_'$pred_len \
+#   --model $model_name \
+#   --data ETTm2 \
+#   --features M \
+#   --seq_len $seq_len \
+#   --pred_len $pred_len \
+#   --enc_in $num_channels \
+#   --des 'Exp' \
+#   --patience 5 \
+#   --train_epochs 100 \
+#   --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
+
+# #Best configuration for ETTm2 and 720 frames horizon
+# pred_len=720
+# python3 -u run_longExp.py \
+#   --activation 'relu' \
+#   --dropout 0.1\
+#   --hidden_size 256\
+#   --num_clusters 10\
+#   --num_blocks 8 \
+#   --is_training 1 \
+#   --root_path ./dataset/ \
+#   --data_path $dataset.csv \
+#   --model_id $dataset'_'$seq_len'_'$pred_len \
+#   --model $model_name \
+#   --data ETTm2 \
+#   --features M \
+#   --seq_len $seq_len \
+#   --pred_len $pred_len \
+#   --enc_in $num_channels \
+#   --des 'Exp' \
+#   --patience 5 \
+#   --train_epochs 100 \
+#   --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
