3515196f453d9e540d2e9e574dd94fcd6732a453
anshul013 <ultimatenanshul@gmail.com>
2025-01-23 14:35:49 +0100

    Changed the param values

--- ETTh1.sh
+++ ETTh1.sh
@@ -18,7 +18,7 @@ num_channels=7
 pred_len=96
 python3 -u run_longExp.py \
   --activation 'relu' \
-  --dropout 0.9 \
+  --dropout 0.2 \
   --num_clusters 3 \
   --clustering_method 'kmeans' \
   --n_layers 6 \
@@ -36,9 +36,33 @@ python3 -u run_longExp.py \
   --pred_len $pred_len \
   --enc_in $num_channels \
   --des 'Exp' \
-  --patience 5 \
+  --patience 10 \
   --train_epochs 100 \
-  --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
+  --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log
+# pred_len=96
+# python3 -u run_longExp.py \
+#   --activation 'relu' \
+#   --dropout 0.9 \
+#   --num_clusters 3 \
+#   --clustering_method 'kmeans' \
+#   --n_layers 6 \
+#   --d_ff 512 \
+#   --in_len $seq_len \
+#   --out_len $pred_len \
+#   --is_training 1 \
+#   --root_path ./dataset/ \
+#   --data_path $dataset.csv \
+#   --model_id $dataset'_'$seq_len'_'$pred_len \
+#   --model $model_name \
+#   --data ETTh1 \
+#   --features M \
+#   --seq_len $seq_len \
+#   --pred_len $pred_len \
+#   --enc_in $num_channels \
+#   --des 'Exp' \
+#   --patience 5 \
+#   --train_epochs 100 \
+#   --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/LongForecasting/$model_name'/'$dataset'_'$seq_len'_'$pred_len.log 
 
 # #Best configuration for ETTm2 and 192 frames horizon
 # pred_len=192
