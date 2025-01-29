11554d3931236fd3ef1aab3d83e07fae3ea8f837
anshul013 <ultimatenanshul@gmail.com>
2025-01-29 14:17:22 +0100

    HCM but with data_loader use due to issues with loading the data

--- exp/exp_hcm.py
+++ exp/exp_hcm.py
@@ -1,15 +1,13 @@
-from data_provider.data_loader import Dataset_MTS
+from data_provider.data_factory import data_provider
 from exp.exp_basic import Exp_Basic
 from models.hcm1.tsmixer import TSMixerH, TMixerH
 from utils.tools import EarlyStopping, adjust_learning_rate
-from utils.ccm.metrics import metric
+from utils.metrics import metric
 
 import numpy as np
 import torch
 import torch.nn as nn
 from torch import optim
-from torch.utils.data import DataLoader
-from torch.nn import DataParallel
 import os
 import time
 import json
@@ -29,59 +27,23 @@ class Exp_HCM(Exp_Basic):
         }
         model = model_dict[self.args.model](self.args).float()
         
-        # Get initial data for clustering
-        train_data, train_loader = self._get_data(flag='train')
-        full_data = []
-        with torch.no_grad():
-            for i, (batch_x, batch_y) in enumerate(train_loader):
-                full_data.append(batch_x)
-        full_data = torch.cat(full_data, dim=0).to(self.device)
-    
-        # Initialize clusters first
-        print("Initializing clusters with full training data...")
-        model.initialize_clusters(full_data)
-        
-        # Print model summary after cluster initialization
-        print("\nModel Architecture:")
-        summary(model)
+        # Print model summary
+        # print("\nModel Architecture:")
+        # summary(model)
         
         return model
 
     def _get_data(self, flag):
-        args = self.args
-
-        if flag == 'test':
-            shuffle_flag = False; drop_last = False; batch_size = args.batch_size
-            data_set = Dataset_MTS(
-                root_path=args.root_path,
-                data_path=args.test_data_path,
-                flag=flag,
-                size=[args.in_len, args.out_len],  
-                data_split=args.test_data_split,
-            )
-        else:
-            shuffle_flag = True; drop_last = False; batch_size = args.batch_size
-            data_set = Dataset_MTS(
-                root_path=args.root_path,
-                data_path=args.data_path,
-                flag=flag,
-                size=[args.in_len, args.out_len],  
-                data_split=args.data_split,
-            )
-
-        print(flag, len(data_set))
-        data_loader = DataLoader(
-            data_set,
-            batch_size=batch_size,
-            shuffle=shuffle_flag,
-            num_workers=args.num_workers,
-            drop_last=drop_last)
-
+        data_set, data_loader = data_provider(self.args, flag)
         return data_set, data_loader
 
     def _select_optimizer(self):
         model_optim = optim.Adam(self.model.parameters(), lr=self.args.learning_rate)
         return model_optim
+    
+    def _select_criterion(self):
+        criterion = nn.MSELoss()
+        return criterion
 
     def vali(self, vali_data, vali_loader):
         self.model.eval()
@@ -90,18 +52,27 @@ class Exp_HCM(Exp_Basic):
         instance_num = 0
         
         with torch.no_grad():
-            for i, (batch_x, batch_y) in enumerate(vali_loader):
-                pred, true = self._process_one_batch(
-                    vali_data, batch_x, batch_y)
-                loss = nn.MSELoss()(pred.detach().cpu(), true.detach().cpu())
+            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(vali_loader):
+                batch_x = batch_x.float().to(self.device)
+                batch_y = batch_y.float().to(self.device)
+                
+                # Forward pass
+                outputs = self.model(batch_x)
+                
+                f_dim = -1 if self.args.features == 'MS' else 0
+                outputs = outputs[:, -self.args.pred_len:, f_dim:]
+                batch_y = batch_y[:, -self.args.pred_len:, f_dim:]
+                
+                loss = nn.MSELoss()(outputs, batch_y)
                 
                 total_loss.append(loss.item())
                 
-                batch_size = pred.shape[0]
+                # Calculate metrics
+                batch_size = outputs.shape[0]
                 instance_num += batch_size
                 
-                pred_np = pred.detach().cpu().numpy()
-                true_np = true.detach().cpu().numpy()
+                pred_np = outputs.detach().cpu().numpy()
+                true_np = batch_y.detach().cpu().numpy()
                 
                 batch_metrics = []
                 for j in range(batch_size):
@@ -122,36 +93,40 @@ class Exp_HCM(Exp_Basic):
                     
     def train(self, setting):
         train_data, train_loader = self._get_data(flag='train')
-        vali_data, vali_loader = self._get_data(flag='val')
-        test_data, test_loader = self._get_data(flag='test')
-    
+        if not self.args.train_only:
+            vali_data, vali_loader = self._get_data(flag='val')
+            test_data, test_loader = self._get_data(flag='test')
+
         path = os.path.join(self.args.checkpoints, setting)
         if not os.path.exists(path):
             os.makedirs(path)
-    
+
         time_now = time.time()
-    
+        
         train_steps = len(train_loader)
         early_stopping = EarlyStopping(patience=self.args.patience, verbose=True)
-    
-        model_optim = self._select_optimizer()
-        criterion = nn.MSELoss()
-
-        # Get full training data for clustering
-        # print("Collecting full training data for clustering initialization...")
-        # full_data = []
-        # with torch.no_grad():
-        #     for i, (batch_x, batch_y) in enumerate(train_loader):
-        #         full_data.append(batch_x)
-        # full_data = torch.cat(full_data, dim=0).to(self.device)
-        # for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(train_loader):
-        #     full_data.append(batch_x)
-        # full_data = torch.cat(full_data, dim=0)  # [total_samples, seq_len, channels]
-    
-        # Initialize clusters with full data once
-        # print("Initializing clusters with full training data...")
-        # self.model.initialize_clusters(full_data)
         
+        model_optim = self._select_optimizer()
+        criterion = self._select_criterion()
+
+        if self.args.use_amp:
+            scaler = torch.cuda.amp.GradScaler()
+
+        # Get initial data for clustering
+        full_data = []
+        with torch.no_grad():
+            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(train_loader):
+                full_data.append(batch_x)
+        full_data = torch.cat(full_data, dim=0).to(self.device)
+
+        # Initialize clusters
+        print("Initializing clusters with full training data...")
+        self.model.initialize_clusters(full_data)
+        
+        # Print model summary with actual data
+        print("\nModel Architecture:")
+        summary(self.model, input_data=full_data, device=self.device)
+
         # Save model architecture and initial settings
         with open(os.path.join(path, "args.json"), 'w') as f:
             json.dump(vars(self.args), f, indent=True)
@@ -162,160 +137,144 @@ class Exp_HCM(Exp_Basic):
         print("Initial clustering metrics:", metrics)
         
         for epoch in range(self.args.train_epochs):
-            epoch_time = time.time()
+            iter_count = 0
             train_loss = []
             
             self.model.train()
-            for i, (batch_x, batch_y) in enumerate(train_loader):
+            epoch_time = time.time()
+            
+            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(train_loader):
+                iter_count += 1
                 model_optim.zero_grad()
                 
-                pred, true = self._process_one_batch(train_data, batch_x, batch_y)
-                loss = criterion(pred, true)
+                batch_x = batch_x.float().to(self.device)
+                batch_y = batch_y.float().to(self.device)
+
+                # Forward pass
+                if self.args.use_amp:
+                    with torch.cuda.amp.autocast():
+                        outputs = self.model(batch_x)
+                        f_dim = -1 if self.args.features == 'MS' else 0
+                        outputs = outputs[:, -self.args.pred_len:, f_dim:]
+                        batch_y = batch_y[:, -self.args.pred_len:, f_dim:]
+                        loss = criterion(outputs, batch_y)
+                else:
+                    outputs = self.model(batch_x)
+                    f_dim = -1 if self.args.features == 'MS' else 0
+                    outputs = outputs[:, -self.args.pred_len:, f_dim:]
+                    batch_y = batch_y[:, -self.args.pred_len:, f_dim:]
+                    loss = criterion(outputs, batch_y)
+
                 train_loss.append(loss.item())
                 
-                loss.backward()
-                model_optim.step()
+                if self.args.use_amp:
+                    scaler.scale(loss).backward()
+                    scaler.step(model_optim)
+                    scaler.update()
+                else:
+                    loss.backward()
+                    model_optim.step()
 
             print("Epoch: {} cost time: {}".format(epoch + 1, time.time() - epoch_time))
             
             train_loss = np.average(train_loss)
-            vali_mse, vali_loss, vali_mae = self.vali(vali_data, vali_loader)
-            test_mse, test_loss, test_mae = self.vali(test_data, test_loader)
+            if not self.args.train_only:
+                vali_mse, vali_loss, vali_mae = self.vali(vali_data, vali_loader)
+                test_mse, test_loss, test_mae = self.vali(test_data, test_loader)
 
-            print("Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Vali Loss: {3:.7f} Test Loss: {4:.7f}".format(
-                epoch + 1, train_steps, train_loss, vali_loss, test_loss))
-            print("Vali MSE: {:.7f}, MAE: {:.7f}, Test MSE: {:.7f}, MAE: {:.7f}".format(
-                vali_mse, vali_mae, test_mse, test_mae))
+                print("Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Vali Loss: {3:.7f} Test Loss: {4:.7f}".format(
+                    epoch + 1, train_steps, train_loss, vali_loss, test_loss))
+                print("Vali MSE: {:.7f}, MAE: {:.7f}, Test MSE: {:.7f}, MAE: {:.7f}".format(
+                    vali_mse, vali_mae, test_mse, test_mae))
 
-            # Save model and early stopping
-            early_stopping(vali_loss, self.model, path)
-            if early_stopping.early_stop:
-                print("Early stopping")
-                break
+                # Save model and early stopping
+                early_stopping(vali_loss, self.model, path)
+                if early_stopping.early_stop:
+                    print("Early stopping")
+                    break
+            else:
+                print("Epoch: {0}, Steps: {1} | Train Loss: {2:.7f}".format(
+                    epoch + 1, train_steps, train_loss))
 
             adjust_learning_rate(model_optim, epoch+1, self.args)
         
         # Load best model
         best_model_path = path + '/' + 'checkpoint.pth'
-        # self.model.load_state_dict(torch.load(best_model_path))
-
-        # Get first batch for initialization
-        # first_batch = next(iter(train_loader))[0]
-        state_dict = torch.load(best_model_path)
-        self.model.load_state_dict(state_dict)
-        # self.model.load_state_dict_with_init(state_dict, first_batch)
-        
-        # Handle DataParallel and save state
-        # state_dict = self.model.module.state_dict() if isinstance(self.model, DataParallel) else self.model.state_dict()
-        # torch.save(state_dict, path + '/' + 'checkpoint.pth')
+        self.model.load_state_dict(torch.load(best_model_path))
         
         return self.model
 
-    def test(self, setting, save_pred=True, inverse=False):
+    def test(self, setting, test=0):
         test_data, test_loader = self._get_data(flag='test')
         
-        self.model.eval()
-        
+        if test:
+            print('loading model')
+            self.model.load_state_dict(torch.load(os.path.join('./checkpoints/' + setting, 'checkpoint.pth')))
+
         preds = []
         trues = []
-        metrics_all = []
-        instance_num = 0
         
+        self.model.eval()
         with torch.no_grad():
-            for i, (batch_x, batch_y) in enumerate(test_loader):
-                pred, true = self._process_one_batch(
-                    test_data, batch_x, batch_y, inverse)
-                batch_size = pred.shape[0]
-                instance_num += batch_size
-                batch_metric = np.array(metric(pred.detach().cpu().numpy(), true.detach().cpu().numpy())) * batch_size
-                metrics_all.append(batch_metric)
-                if save_pred:
-                    preds.append(pred.detach().cpu().numpy())
-                    trues.append(true.detach().cpu().numpy())
+            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(test_loader):
+                batch_x = batch_x.float().to(self.device)
+                batch_y = batch_y.float().to(self.device)
+                
+                outputs = self.model(batch_x)
+                
+                f_dim = -1 if self.args.features == 'MS' else 0
+                outputs = outputs[:, -self.args.pred_len:, f_dim:]
+                batch_y = batch_y[:, -self.args.pred_len:, f_dim:]
+                
+                pred = outputs.detach().cpu().numpy()
+                true = batch_y.detach().cpu().numpy()
+                
+                preds.append(pred)
+                trues.append(true)
 
-        metrics_all = np.stack(metrics_all, axis=0)
-        metrics_mean = metrics_all.sum(axis=0) / instance_num
-
+        preds = np.concatenate(preds, axis=0)
+        trues = np.concatenate(trues, axis=0)
+        
         # Save results
         folder_path = './results/' + setting +'/'
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
 
-        mae, mse, rmse, mape, mspe = metrics_mean
+        mae, mse, rmse, mape, mspe = metric(preds, trues)
         print('mse:{}, mae:{}'.format(mse, mae))
 
         np.save(folder_path+'metrics.npy', np.array([mae, mse, rmse, mape, mspe]))
-        if save_pred:
-            preds = np.concatenate(preds, axis=0)
-            trues = np.concatenate(trues, axis=0)
-            np.save(folder_path+'pred.npy', preds)
-            np.save(folder_path+'true.npy', trues)
+        np.save(folder_path+'pred.npy', preds)
+        np.save(folder_path+'true.npy', trues)
+        
         return
 
-    def _process_one_batch(self, dataset_object, batch_x, batch_y, inverse=False):
-        batch_x = batch_x.float().to(self.device)
-        batch_y = batch_y.float().to(self.device)
+    def predict(self, setting, load=False):
+        pred_data, pred_loader = self._get_data(flag='pred')
         
-        outputs = self.model(batch_x)
+        if load:
+            path = os.path.join(self.args.checkpoints, setting)
+            best_model_path = path + '/' + 'checkpoint.pth'
+            self.model.load_state_dict(torch.load(best_model_path))
 
-        if inverse:
-            outputs = dataset_object.inverse_transform(outputs)
-            batch_y = dataset_object.inverse_transform(batch_y)
-        return outputs, batch_y
-
-    def eval(self, setting, save_pred=True, inverse=False):
-        args = self.args
-        data_set = Dataset_MTS(
-            root_path=args.root_path,
-            data_path=args.data_path,
-            flag='test',
-            size=[args.in_len, args.out_len],  
-            data_split=args.data_split,
-            scale=True,
-            scale_statistic=args.scale_statistic,
-        )
-
-        data_loader = DataLoader(
-            data_set,
-            batch_size=args.batch_size,
-            shuffle=False,
-            num_workers=args.num_workers,
-            drop_last=False)
+        preds = []
         
         self.model.eval()
-        
-        preds = []
-        trues = []
-        metrics_all = []
-        instance_num = 0
-        
         with torch.no_grad():
-            for i, (batch_x, batch_y) in enumerate(data_loader):
-                pred, true = self._process_one_batch(
-                    data_set, batch_x, batch_y, inverse)
-                batch_size = pred.shape[0]
-                instance_num += batch_size
-                batch_metric = np.array(metric(pred.detach().cpu().numpy(), true.detach().cpu().numpy())) * batch_size
-                metrics_all.append(batch_metric)
-                if save_pred:
-                    preds.append(pred.detach().cpu().numpy())
-                    trues.append(true.detach().cpu().numpy())
-
-        metrics_all = np.stack(metrics_all, axis=0)
-        metrics_mean = metrics_all.sum(axis=0) / instance_num
-
-        # Save results
-        folder_path = './results/' + setting +'/'
+            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(pred_loader):
+                batch_x = batch_x.float().to(self.device)
+                outputs = self.model(batch_x)
+                pred = outputs.detach().cpu().numpy()
+                preds.append(pred)
+        
+        preds = np.array(preds)
+        preds = np.concatenate(preds, axis=0)
+        
+        # result save
+        folder_path = './results/' + setting + '/'
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
-
-        mae, mse, rmse, mape, mspe = metrics_mean
-        print('Evaluation metrics - MSE: {}, MAE: {}'.format(mse, mae))
-
-        np.save(folder_path+'metrics.npy', np.array([mae, mse, rmse, mape, mspe]))
-        if save_pred:
-            preds = np.concatenate(preds, axis=0)
-            trues = np.concatenate(trues, axis=0)
-            np.save(folder_path+'pred.npy', preds)
-            np.save(folder_path+'true.npy', trues)
+        
+        np.save(folder_path + 'real_prediction.npy', preds)
+        
         return 
